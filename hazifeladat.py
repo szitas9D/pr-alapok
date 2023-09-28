6cd917ffd7f043bca60453f7a53606c526760a38
szitas9D <126696525+szitas9D@users.noreply.github.com>
2023-09-28 09:21:36 +0200

    Add files via upload

--- hazifeladat.py
+++ hazifeladat.py
@@ -1,7 +1,7 @@
 válasz=input("Jó napod van ma?")
-if=="igen":
+if válasz=="igen":
   print("Örülök neki")
-  elif válasz=="nem" :
+elif válasz=="nem" :
       print("Sajnálom")
 else:
     print("Erre nem tudok választ adni sajnálom")
